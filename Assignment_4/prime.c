1d9e9039592feaaa8b767eaca7a68c84148e8c1a
YogeshMane2003 <yogeshmane71011@gmail.com>
2025-10-11 13:37:39 +0530

    Add files via upload

---
+++ Assignment_4/prime.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main() {
+    int n = 50;    
+    int i, j, flag;
+
+    printf("Prime numbers:\n");
+
+    for(i = 2; i <= n; i++) {      
+        flag = 0;                  
+
+        for(j = 2; j <= i / 2; j++) {  
+            if(i % j == 0) {
+                flag = 1;          
+                break;
+            }
+        }
+
+        if(flag == 0) {            
+            printf("%d ", i);
+        }
+    }
+}
