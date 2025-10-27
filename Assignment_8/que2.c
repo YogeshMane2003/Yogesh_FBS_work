f0bf3f6e925b7760e5d3817f086530831043d79c
YogeshMane2003 <yogeshmane71011@gmail.com>
2025-10-27 23:17:31 +0530

    Add files via upload

---
+++ Assignment_8/que2.c
@@ -0,0 +1,37 @@
+// Search the given number in array.
+
+#include<stdio.h>
+
+void main(){
+int arr[5];
+
+
+printf("Enter the elements:");
+
+for(int i=0; i<5; i++){
+    scanf("%d",&arr[i]);
+}
+
+for(int i=0; i<5;i++){
+    printf("%d ",arr[i]);
+}
+
+int num;
+int flag = 0,index;
+printf("\nEnter the number to search:");
+scanf("%d",&num);
+
+for(int i=0;i<5;i++){
+    if(num == arr[i]){
+        flag = 1;
+        index = i;
+        break;
+    }
+}
+if(flag == 1)
+{
+printf("Found at %d index",index);
+}else{
+    printf("Not found");
+}
+}
